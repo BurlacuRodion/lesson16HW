90cc245bafc3dca8f3d35b818a0fc7d459a46914
Burlacu Rodion <burlacurodionmd@gmail.com>
2024-04-07 12:30:08 +0300

    add functions

--- index.js
+++ index.js
@@ -2,17 +2,32 @@ for (let i=0; i<=10;i++) {
     //console.log(i)
 }
 
-//----------------------------//
-
 let i = 0;
 while(i<=10) {
     //console.log(i++)
 }
 
+let j = 0;
+do {
+   // console.log(j++)
+} while (j<=10)
+
+//---------------------------//
+
+let str = 'abcdefg'
+for (let i=0;i<str.length;i++) {
+    //console.log(str[i])
+}
 //----------------------------//
 
-let j = 0;
+let arrFunc = (str) => {
+    for (let i=0;i<str.length;i++) console.log('arrFunc', str[i])
+}
 
-do {
-    console.log(j++)
-} while (j<=10)
+function expFunc(str) {
+    for (let i=0;i<str.length;i++) console.log('expFunc', str[i])
+}
+
+const decFunc = function(str) {
+    for (let i=0;i<str.length;i++) console.log('decFunc', str[i])
+}
