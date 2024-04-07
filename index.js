891627c9d04a017050a86a63d7219ceda7c9c573
Burlacu Rodion <burlacurodionmd@gmail.com>
2024-04-07 12:04:00 +0300

    add for,while, do while loop

---
+++ index.js
@@ -0,0 +1,18 @@
+for (let i=0; i<=10;i++) {
+    //console.log(i)
+}
+
+//----------------------------//
+
+let i = 0;
+while(i<=10) {
+    //console.log(i++)
+}
+
+//----------------------------//
+
+let j = 0;
+
+do {
+    console.log(j++)
+} while (j<=10)
